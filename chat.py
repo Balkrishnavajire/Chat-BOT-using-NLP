577b4f8051808029eff38df5c2b64022004acaca
Balkrishna vajire <57069207+Balkrishnavajire@users.noreply.github.com>
2022-11-17 20:50:10 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,81 @@
+import random
+import json
+import speech_recognition as sr
+import pyttsx3
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+print('Loading your ONOBOT')
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+speak("Loading your- ONOBOT")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "ONOBOT"
+print("Let's chat! ( quit to exit)")
+while True:
+    # sentence = "do you use credit cards?"
+    sentence = takeCommand().lower()
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
